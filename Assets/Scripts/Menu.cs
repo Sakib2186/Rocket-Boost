3f91ca1d0487d1b9e99961f474c3a04ee22707d7
Sakib2186 <skmdsakib2186@gmail.com>
2026-01-17 12:47:51 +0600

    Added new UI and text rendering

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-{ 
+{
+    [SerializeField] AudioClip clip;
+    AudioSource ads;
+
+    private void Start()
+    {
+        ads = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!ads.isPlaying)
+        {
+            ads.PlayOneShot(clip);
+        }
+        
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
